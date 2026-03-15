311ec9cf7725ecde45ee4985ba167a152806d28f
H.Ganesh Kumar <192572027.simats@saveetha.com>
2026-03-15 11:39:39 +0530

    Create A203.c

---
+++ A203.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    int a[50],n,pos,val,i;
+
+    printf("Enter size: ");
+    scanf("%d",&n);
+
+    printf("Enter elements:\n");
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+
+    printf("Enter position: ");
+    scanf("%d",&pos);
+
+    printf("Enter value: ");
+    scanf("%d",&val);
+
+    for(i=n;i>=pos;i--)
+        a[i]=a[i-1];
+
+    a[pos-1]=val;
+    n++;
+
+    printf("Array after insertion:\n");
+    for(i=0;i<n;i++)
+        printf("%d ",a[i]);
+}
